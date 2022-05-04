2f992d5721471dba5c93afe32bd2f896b7497323
leansparrow <dmitry.vorobyev@gmail.com>
2022-05-04 22:05:48 +0300

    Конструктор и Инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -4,34 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Point p1=new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sim = '*';
+            Point p1=new Point(1,3,'*');
             p1.Draw();
             
-            /*int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';*/
-
-            /*Draw(p1.x,p1.y,p1.sim);*/
-
-            Point p2=new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sim = '#';
+            Point p2=new Point(4,5,'#');
             p2.Draw();
-            /*int x2 = 4;
-            int y2 = 5;
-            char sim2 = '#';*/
-            /*Draw(p2.x, p2.y, p2.sim);*/
-
+            
             Console.ReadLine();
         }
-        /*static void Draw(int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
-        }*/
     }
 }
